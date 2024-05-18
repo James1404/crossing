941ad4dd2515ae3f7f3599212fdfb50f4ea6c465
James <jamesbarnfather99@gmail.com>
2024-05-18 11:18:00 +0100

    started type checker

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,24 @@
 import scala.collection.mutable.ArrayBuffer
 import scala.compiletime.ops.double
+import scala.collection.mutable.Stack
+import scala.compiletime.ops.float
+import scala.compiletime.ops.string
+import scala.compiletime.ops.boolean
+import scala.collection.immutable.HashMap
 
-type ReservedSymbol = '=' | ':' | ';' | '(' | ')' | '{' | '}'
-type ReservedKeyword = "fn" | "type" | "if" | "else" | "match"
+type ReservedSymbol = '=' | ';' | '(' | ')' | '{' | '}'
+type ReservedKeyword =
+    "->"
+  | "::"
+  | "fn" | "type"
+  | "if" | "else"
+  | "true" | "false"
+  | "match"
   
 enum TokenType {
   case Invalid
   case Identifier(id: String)
-  case Number(v: Int | Float)
+  case Number(v: Float)
   case Keyword(ty: ReservedKeyword)
   case Symbol(ty: ReservedSymbol)
   case EOF
@@ -75,21 +86,24 @@ enum AST {
   case Global(items: Array[AST])
   case Scope(items: Array[AST])
 
-  case NumberLiteral(num: Token)
-  case StringLiteral(str: Token)
-  case Identifier(id: Token)
+  case NumberLiteral(t: Token, v: Float)
+  case StringLiteral(t: Token, v: String)
+  case Identifier(t: Token, v: String)
 
-  case List(items: Array[AST])
+  case Application(items: Array[AST])
   
   case InfixOperator(lhs: AST, op: AST, rhs: AST)
   
-  case FnDecleration(id: AST, params: Array[AST], body: AST)
+  case TypedAbstraction(id: AST, params: Array[AST])
+  case Abstraction(id: AST, params: Array[AST], body: AST)
   
   case TypeDecleration(variants: Array[AST])
   case TypeRecord(variants: Array[(AST, AST)])
   case TypeAlias(ty: AST)
   
   case IfExpression(cond: AST, true_block: AST, false_block: AST)
+  
+  case Pattern(cond: AST, body: AST)
   
   case MatchCase(pattern: AST, value: AST)
   case MatchExpression(value: AST, patterns: Array[MatchCase])
@@ -115,10 +129,10 @@ class Parser(tokens: ArrayBuffer[Token]) {
   def value: AST = 
     val c = current
     c.ty match
-      case TokenType.Number(_) => advance; AST.NumberLiteral(c)
-      case TokenType.Identifier(_) =>
+      case TokenType.Number(v) => advance; AST.NumberLiteral(c, v)
+      case TokenType.Identifier(v) =>
         advance
-        AST.Identifier(c)
+        AST.Identifier(c, v)
       case TokenType.Symbol('(') => advance; expr(TokenType.Symbol(')'))
       case _ =>
         errors.addOne(LangError("Invalid Token"))
@@ -138,8 +152,11 @@ class Parser(tokens: ArrayBuffer[Token]) {
   //     lhs = AST.Binary(lhs, op, rhs)
   //   lhs
   
-  def identifier =
-    val id = AST.Identifier(current)
+  def identifier: Option[AST] =
+    val id = current.ty match
+      case TokenType.Identifier(id) =>
+        Some(AST.Identifier(current, id))
+      case _ => None
     advance
     id
   
@@ -149,41 +166,94 @@ class Parser(tokens: ArrayBuffer[Token]) {
       list.addOne(value)    
     
     advance
-    AST.List(list.toArray)
+    AST.Application(list.toArray)
 
-  def fn_decleration: AST =
+  def typed_abstraction: Option[AST] =
     if current.ty == TokenType.Keyword("fn") then
       advance
-      val id = identifier;
+      val id = identifier getOrElse AST.Invalid;
       var params = ArrayBuffer[AST]();
-      while current.ty != TokenType.Symbol('=') do params.addOne(identifier)
+      if current.ty == TokenType.Keyword("::") then
+        return Some(AST.TypedAbstraction(id, params.toArray))
+
+    None
+
+  def abstraction: Option[AST] =
+    if current.ty == TokenType.Keyword("fn") then
+      advance
+      val id = identifier getOrElse AST.Invalid;
+      var params = ArrayBuffer[AST]();
+      
+      while current.ty != TokenType.Symbol('=') do params.addOne(identifier getOrElse AST.Invalid)
+
       advance
       val body = expr;
-      
-      AST.FnDecleration(id, params.toArray, expr(TokenType.Symbol(';')))
-    else
-      AST.Invalid
+        
+      return Some(AST.Abstraction(id, params.toArray, expr(TokenType.Symbol(';'))))
+
+    None
   
-  def type_decleration: AST = AST.Invalid
+  def type_decleration: Option[AST] =
     if current.ty == TokenType.Keyword("type") then
       advance
-      val id = identifier;
-      AST.Invalid
+      val id = identifier getOrElse AST.Invalid;
+      Some(AST.Invalid)
     else
-      AST.Invalid
+      None
   
   def toplevel: AST =
     val scope = ArrayBuffer[AST]();
     while safe do
-      current.ty match
-        case TokenType.Keyword("fn") => scope.addOne(fn_decleration)
-        case TokenType.Keyword("type") => scope.addOne(type_decleration)
-        case _ => ()
+      abstraction.getOrElse(typed_abstraction.getOrElse(type_decleration.getOrElse(AST.Invalid)))
     
     AST.Global(scope.toArray);
     
   def run = toplevel
 }
 
+enum Type {
+  case None
+  
+  case Unit
+
+  case Number
+  case String
+  case Bool
+  case Function(in: Array[Type], out: Type)
+}
+
+class ScopeVar(val id: String, val scope: Int, val ty: Type)
+class TypeContext {
+  val scope = ArrayBuffer[ScopeVar]();
+  var currentScope = 0;
+  
+  def increment: Unit = currentScope += 1;
+  def decrement: Unit =
+    currentScope -= 1
+    if !scope.isEmpty then
+      while scope.last.scope > currentScope do scope.remove(scope.length - 1)
+}
+
 class Typechecker(tree: AST) {
+  def check: Type = Type.None
+  def infer: Type = Type.None
+}
+
+enum Value {
+  case Number(value: Float)
+  case String(value: String)
+  case Boolean(value: Boolean)
+  case Function
+}
+
+class Interpreter(tree: AST) {
+  val stack = Stack[Value]()
+  
+  def exec(node: AST): Unit = node match
+    case AST.Application(items) =>
+      val function = items(0)
+    case AST.NumberLiteral(_, num) => stack.push(Value.Number(num))
+    case _ => ()
+
+  def run: Unit = ()
 }
